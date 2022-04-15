36dfe0e2d3dc197d7a99e1118ba6ee62a65f7239
Thoma77s <thomassiffc3@gmail.com>
2022-04-15 16:04:30 -0400

    got 5 cards to show

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var locationInputEl = document.querySelector("#location-input");
 var cityFormEl = document.querySelector("#city-form");
-var cityContainerEl = document.querySelector(".city-container")
-var savedLocationsEl = document.querySelector(".saved-locations")
+var cityContainerEl = document.querySelector(".city-container");
+var savedLocationsEl = document.querySelector(".saved-locations");
+var weekContainerEl = document.querySelector(".week-container");
+
 
 
 var getWeather = function(city) {
@@ -11,6 +13,25 @@ var getWeather = function(city) {
   fetch(apiUrl).then(function(response) {
     if (response.ok) {
     response.json().then(function(city) {
+       var responseLat = city.coord.lat;
+       var responseLon = city.coord.lon;
+       var coordinates = responseLat,responseLon;
+      console.log(responseLat);
+      console.log(responseLon);
+      var apiUrlCoordinates = "https://api.openweathermap.org/data/2.5/onecall?lat=" + city.coord.lat + "&lon="+ city.coord.lon + "&limit=5&appid=dc2556c07508f18009a5420cc2296743";
+        fetch(apiUrlCoordinates).then(function(response) {
+          if (response.ok) {
+          response.json().then(function(city) {
+      
+          });
+        }else {
+          window.alert("Error: City Not Found");
+        }
+        })
+        .catch(function(error) {
+          alert("Unable to connect to Database");
+        });
+      //getDayWeather(city);
       console.log(city);
       displayWeather(city);
     });
@@ -21,11 +42,45 @@ var getWeather = function(city) {
   .catch(function(error) {
     alert("Unable to connect to Database");
   });
-  }
 
-  var formSubmitHandler = function(event) {
+  var getUrlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=dc2556c07508f18009a5420cc2296743";
+  fetch(getUrlFiveDay).then(function(response) {
+    if (response.ok) {
+    response.json().then(function(city) {
+      // var responseLat = city.coord.lat;
+      // var responseLon = city.coord.lon;
+      // var coordinates = responseLat,responseLon;
+      console.log(city)
+      var apiUrlCoordinates = "https://api.openweathermap.org/data/2.5/onecall?lat=" + city.coord.lat + "&lon="+ city.coord.lon + "&limit=5&appid=dc2556c07508f18009a5420cc2296743";
+        fetch(apiUrlCoordinates).then(function(response) {
+          if (response.ok) {
+          response.json().then(function(city) {
+      
+          });
+        }else {
+          window.alert("Error: City Not Found");
+        }
+        })
+        .catch(function(error) {
+          alert("Unable to connect to Database");
+        });
+      //getDayWeather(city);
+      console.log(city);
+      displayWeather(city);
+    });
+  }else {
+    window.alert("Error: City Not Found");
+  }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to Database");
+  });
+    
+}
+
+var formSubmitHandler = function(event) {
     const citiesContainer = document.getElementById('city-container');
-    citiesContainer.innerHTML = "";
+    //citiesContainer.innerHTML = "";
     event.preventDefault();
     var city = locationInputEl.value.trim();
   
@@ -36,7 +91,7 @@ var getWeather = function(city) {
       alert("Please enter a city");
     }
     console.log(event);
-  }
+}
 
   var displayWeather = function(city) {
     // check if api returned anything
@@ -45,21 +100,40 @@ var getWeather = function(city) {
       return;
     }  
      createCityCards(city);
+     createDayCards(city)
+    
   };
 
    function createCityCards(city) { 
     const citiesContainer = document.getElementById('city-container');
      // Select the card-template in the DOM
      const cardTemplate = document.getElementById('card-template');
-     city.forEach((city) => {
+     console.log(city.main.temp);
        // Create a cardTemplate element in javascript to start populating data
        const card = document.importNode(cardTemplate.content, true);
        card.querySelector('.city-name').textContent = city.name;
-       card.querySelector('.city-temp').textContent = city.temp;
-       card.querySelector('.city-wind').textContent = city.wind;
-       card.querySelector('.city-humidity').textContent = city.humidity;
+       card.querySelector('.city-temp').textContent = "Temp: " + (((city.main.temp-273.15)*1.8)+32).toFixed(2);;
+       card.querySelector('.city-wind').textContent = "Wind: " + city.wind.speed + " MPH";
+       card.querySelector('.city-humidity').textContent = "Humidity: " + city.main.humidity + " %";
+       //card.querySelector('.city-uvi').textContent = "UV-Index: " + city.coord.lat,city.coord.lon.current.uvi
        citiesContainer.appendChild(card);
-     });
    };
 
-  cityFormEl.addEventListener("submit", formSubmitHandler);
+   function createDayCards(city) { 
+    const citiesContainer = document.getElementById('week-container');
+     // Select the card-template in the DOM
+     const cardTemplate = document.getElementById('day-card-template');
+     for(i=0; i < 6; i++) {
+       // Create a cardTemplate element in javascript to start populating data
+       const card = document.importNode(cardTemplate.content, true);
+       console.log(city.list);
+       card.querySelector('.day-of-the-week').textContent = city.name;
+       card.querySelector('.day-city-temp').textContent = "Feels like: " + (((city.main.feels_like.day-273.15)*1.8)+32).toFixed(2);
+       card.querySelector('.day-city-wind').textContent = "Wind: " + city.wind.speed + " MPH";
+       card.querySelector('.day-city-humidity').textContent = "Humidity: " + city.main.humidity + " %";
+       citiesContainer.appendChild(card);
+  };
+}
+
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
